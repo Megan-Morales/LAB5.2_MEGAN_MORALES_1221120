ce2840723fab4e31aee89837761c062bd289aa25
Megan-Morales <63917946+Megan-Morales@users.noreply.github.com>
2021-10-10 13:05:34 -0600

    Interfaz ejercicio 2

--- Ej2.h
+++ Ej2.h
@@ -34,6 +34,8 @@ namespace LAB52MEGANMORALES1221120 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,16 +50,30 @@ namespace LAB52MEGANMORALES1221120 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"MV Boli", 25.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(190, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(838, 58);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"BIENVENIDO AL SOLITARIO SIMPLE";
 			// 
 			// Ej2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(869, 406);
+			this->ClientSize = System::Drawing::Size(1207, 565);
+			this->Controls->Add(this->label1);
 			this->Name = L"Ej2";
 			this->Text = L"Ej2";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
