340d6e1fbb28fceaa197930a65f31b5306d56ae7
Megan-Morales <63917946+Megan-Morales@users.noreply.github.com>
2021-10-10 19:32:27 -0600

    Interfaz 2

--- Ej2.h
+++ Ej2.h
@@ -35,6 +35,18 @@ namespace LAB52MEGANMORALES1221120 {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ btRegresar;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ btR;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Button^ btMazo;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
 	protected:
 
 	private:
@@ -51,27 +63,166 @@ namespace LAB52MEGANMORALES1221120 {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->btRegresar = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->btMazo = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->btR = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"MV Boli", 25.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"PLAY FLOLALIA", 25.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(190, 9);
+			this->label1->Location = System::Drawing::Point(42, 34);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(838, 58);
+			this->label1->Size = System::Drawing::Size(533, 50);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"BIENVENIDO AL SOLITARIO SIMPLE";
+			// 
+			// btRegresar
+			// 
+			this->btRegresar->Location = System::Drawing::Point(969, 34);
+			this->btRegresar->Name = L"btRegresar";
+			this->btRegresar->Size = System::Drawing::Size(75, 30);
+			this->btRegresar->TabIndex = 1;
+			this->btRegresar->Text = L"Regresar";
+			this->btRegresar->UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::GradientActiveCaption;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(13, 99);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(704, 324);
+			this->dataGridView1->TabIndex = 2;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->btR);
+			this->groupBox1->Controls->Add(this->textBox4);
+			this->groupBox1->Controls->Add(this->btMazo);
+			this->groupBox1->Controls->Add(this->textBox3);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Controls->Add(this->dataGridView1);
+			this->groupBox1->Location = System::Drawing::Point(38, 103);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(1006, 446);
+			this->groupBox1->TabIndex = 3;
+			this->groupBox1->TabStop = false;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(882, 179);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(882, 218);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 22);
+			this->textBox2->TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(882, 260);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 22);
+			this->textBox3->TabIndex = 5;
+			// 
+			// btMazo
+			// 
+			this->btMazo->Location = System::Drawing::Point(13, 58);
+			this->btMazo->Name = L"btMazo";
+			this->btMazo->Size = System::Drawing::Size(71, 24);
+			this->btMazo->TabIndex = 6;
+			this->btMazo->Text = L"MAZO: ";
+			this->btMazo->UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(90, 58);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 22);
+			this->textBox4->TabIndex = 7;
+			// 
+			// btR
+			// 
+			this->btR->Location = System::Drawing::Point(116, 29);
+			this->btR->Name = L"btR";
+			this->btR->Size = System::Drawing::Size(51, 23);
+			this->btR->TabIndex = 8;
+			this->btR->Text = L"R";
+			this->btR->UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Papyrus", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(795, 177);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(81, 24);
+			this->label2->TabIndex = 9;
+			this->label2->Text = L"Posición: ";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Papyrus", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(804, 218);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(72, 24);
+			this->label3->TabIndex = 10;
+			this->label3->Text = L"Origen: ";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Papyrus", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(795, 258);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(80, 24);
+			this->label4->TabIndex = 11;
+			this->label4->Text = L"Destino: ";
+			this->label4->TextAlign = System::Drawing::ContentAlignment::TopCenter;
 			// 
 			// Ej2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1207, 565);
+			this->ClientSize = System::Drawing::Size(1084, 614);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->btRegresar);
 			this->Controls->Add(this->label1);
 			this->Name = L"Ej2";
 			this->Text = L"Ej2";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
